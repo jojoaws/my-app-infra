629cbe47437764a04141e3eaa7c51b841b0a35ef
jojo <elokajosephine10@gmail.com>
2026-05-17 14:34:10 -0700

    add ECR force delete

--- ecr.tf
+++ ecr.tf
@@ -1,7 +1,11 @@
 resource "aws_ecr_repository" "app_repo" {
   name = "my-app"
+
+  force_delete = true
 }
 
 resource "aws_ecr_repository" "worker_repo" {
   name = "worker"
+
+  force_delete = true
 }
