a61d47ce9b3e828cb4e987568103683a80c6567e
jojo <elokajosephine10@gmail.com>
2026-05-20 17:32:31 -0700

    configure s3 remote backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "cloud-mastery-tfstate-bucket"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
+    bucket = "cloud-mastery-tfstate-bucket"
+    key    = "dev/terraform.tfstate"
+    region = "us-east-1"
   }
 }
